11e831a37af3cd47474e9b4066b5ad934b13866a
Cody Merritt <cody.merritt@icloud.com>
2020-08-05 23:12:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array [2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|name| name[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|int| sum += int}
+  sum
+end
+
+def sum_array_with_inject(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.collect {|word| word << "s"}
+  array[1].chop!
+  array
+end
