70847d0e16262275cc85d0e5c107d2591fd37637
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-05 06:24:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+ array.sort_by{|x| x.length}
+end
+
+def swap_elements(array)
+  array = array.sort_by {|x| x.length}
+end 
+
+def reverse_array(array)
+  copy = array.reverse 
+  return copy
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end 
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, x| sum + x}
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index| index == 1 ? element : element[element.length] = "s"}
+  array
+end
